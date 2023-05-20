5080111a8f3bb92b94fe90fa1c650408650bd8cf
Miguel Santos <miguel.santos@etml-es.ch>
2023-05-20 14:53:21 +0200

    Creation projet MPLAB

---
+++ MINF-REVISION/firmware/MINF-REVISION.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=MINF-REVISION.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MINF-REVISION.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=minf-revision.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/minf-revision.x.tar
